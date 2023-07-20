d4dbdad2d3c55444241a4a42889d064166f5da7c
Ovídio Orsi <ovidio.orsi@gmail.com>
2023-07-20 13:39:27 -0300

    ScrollSuave transformado em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,17 +1,23 @@
 // SCROLL SUAVE LINK INTERNO
-export default function initScrollSuave() {
-    const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+export default class ScrollSuave {
+    constructor(links, options) {
+        this.linksInternos = document.querySelectorAll(links);
+        if(options === undefined) {
+            this.options = { behavior: 'smooth', block: 'start' };
+        } else {
+            this.options = options;
+        }
 
-    function scrollToSection(event) {
+        this.scrollToSection = this.scrollToSection.bind(this);
+    }
+
+    scrollToSection(event) {
         event.preventDefault();
         const href = event.currentTarget.getAttribute('href');
         const section = document.querySelector(href);
         // const topo = section.offsetTop;
 
-        section.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-        });
+        section.scrollIntoView(this.options);
 
         // FORMA ALTERNATIVA
         // window.scrollTo({
@@ -20,7 +26,16 @@ export default function initScrollSuave() {
         // });
     }
 
-    linksInternos.forEach((link) => {
-        link.addEventListener('click', scrollToSection)
-    })
+    addLinkEvent() {
+        this.linksInternos.forEach((link) => {
+            link.addEventListener('click', this.scrollToSection)
+        })
+    }
+
+    init() {
+        if(this.linksInternos.length) {
+            this.addLinkEvent();
+        }
+        return this;
+    }
 }
